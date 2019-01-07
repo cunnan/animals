8abdddc6ed550ef9751710777c3bdfe0af2a074a
“cunnan” <cunnan>
2019-01-07 23:24:08 +0800

    animals init 20190107

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+var animals = express();
+
+animals.listen(5050,()=>{
+    console.log('animals:5050 listening...');
+});
+
+animals.use(express.static('./public'));
+
+animals.get('/',(req,res)=>{
+    res.redirect('html');
+});
