3c79790418b89e20f81b23c34468822805ae6814
cunnan <cunnan@animals.com>
2019-01-15 22:48:27 +0800

    20190115项目前端基础搭建

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ animals.listen(5050,()=>{
     console.log('animals:5050 listening...');
 });
 
-animals.use(express.static('./public'));
+animals.use(express.static('public'));
 
 animals.get('/',(req,res)=>{
     res.redirect('html');
